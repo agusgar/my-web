40a3c80c6608aceeeab5e084762475e15a52aa55
Agustin Garbarino <agusgarba@gmail.com>
2021-06-27 12:26:45 +0200

    Add eslint, prettier and materialui

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,72 @@
 module.exports = {
   siteMetadata: {
-    title: "My Web",
+    title: 'My Web',
   },
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: "v_dj7QdLiQaaX_zqLdMdDXbxEGQybyMW29tgKRotMes",
-        spaceId: "taparbhh4o58",
+        accessToken: 'v_dj7QdLiQaaX_zqLdMdDXbxEGQybyMW29tgKRotMes',
+        spaceId: 'taparbhh4o58',
       },
     },
-    "gatsby-plugin-image",
+    'gatsby-plugin-image',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "200608376",
+        trackingId: '200608376',
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/',
       },
-      __key: "pages",
+      __key: 'pages',
     },
+    {
+      resolve: 'gatsby-plugin-prettier-eslint',
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}',
+          ],
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
+    'gatsby-plugin-material-ui',
   ],
 };
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+// markup
+const IndexPage = () => (
+  <main>
+    <title>Home Page</title>
+  </main>
+);
+
+export default IndexPage;
